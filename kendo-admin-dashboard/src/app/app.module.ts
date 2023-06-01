9ec122bfb381f74dcfcd8767860b6b126b88a5df
DanielTakev <daniel.takev.vm@gmail.com>
2023-06-01 13:17:47 +0300

    Updated project structure and resources
    Updated index.html

--- kendo-admin-dashboard/src/app/app.module.ts
+++ kendo-admin-dashboard/src/app/app.module.ts
@@ -1,20 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
+import { GridModule, PDFModule, ExcelModule } from '@progress/kendo-angular-grid';
+import { ChartsModule } from '@progress/kendo-angular-charts';
+import { InputsModule } from '@progress/kendo-angular-inputs';
+
 import { AppComponent } from './app.component';
-import { GridModule } from '@progress/kendo-angular-grid';
+
+import 'hammerjs';
+import {RatingComponent} from "./rating.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    GridModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    GridModule,
+    ChartsModule,
+    InputsModule,
+    PDFModule,
+    ExcelModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    RatingComponent
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
--- kendo-admin-dashboard/src/main.ts
+++ kendo-admin-dashboard/src/main.ts
@@ -1,7 +1,16 @@
+import './polyfills';
+
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+platformBrowserDynamic().bootstrapModule(AppModule, { preserveWhitespaces: true }).then(ref => {
+  // Ensure Angular destroys itself on hot reloads.
+  const _window = window as any;
+  if (_window['ngRef']) {
+    _window.destroy();
+  }
+  _window['ngRef'] = ref;
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+  // Otherwise, log the boot error
+}).catch(err => console.error(err));
